8ace375e2eb281d383ebbf991c333a9112accd54
lll <505053397@qq.com>
2017-02-08 15:01:58 +0800

    推荐界面的布局

---
+++ douyu/douyu/Classes/Home/Controller/RecommendViewController.swift
@@ -0,0 +1,88 @@
+//
+//  RecommendViewController.swift
+//  douyu
+//
+//  Created by 练锦波 on 2017/2/8.
+//  Copyright © 2017年 练锦波. All rights reserved.
+//
+
+import UIKit
+
+private let kItemMargin : CGFloat = 10
+private let kItemW = (kScreenW - 3 * kItemMargin) / 2
+private let kItemH = kItemW * 3 / 4
+private let kHeaderViewH : CGFloat = 50
+
+private let kNormalCellID = "kNormalCellID"
+private let kHeaderViewID = "kHeaderViewID"
+
+class RecommendViewController: UIViewController {
+
+    // MARK: - 懒加载属性
+    fileprivate lazy var collectionView : UICollectionView = {[unowned self] in
+        // 1.创建布局
+        let layout = UICollectionViewFlowLayout()
+        layout.itemSize = CGSize(width: kItemW, height: kItemH)
+        layout.minimumLineSpacing = 0
+        layout.minimumInteritemSpacing = kItemMargin
+        layout.headerReferenceSize = CGSize(width: kScreenW, height: kHeaderViewH)
+        layout.sectionInset = UIEdgeInsets(top: 0, left: kItemMargin, bottom: 0, right: kItemMargin)
+        
+        // 2.创建uicollectionview
+        let collectionView = UICollectionView(frame: self.view.bounds, collectionViewLayout: layout)
+        collectionView.backgroundColor = UIColor.blue
+        collectionView.dataSource = self
+        collectionView.autoresizingMask = [.flexibleHeight, .flexibleWidth]
+        
+        collectionView.register(UICollectionViewCell.self, forCellWithReuseIdentifier: kNormalCellID)
+        collectionView.register(UICollectionReusableView.self, forSupplementaryViewOfKind: UICollectionElementKindSectionHeader, withReuseIdentifier: kHeaderViewID)
+        
+        return collectionView
+    }()
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // 设置UI界面
+        setupUI()
+        
+        
+    }
+}
+
+// MARK:- 设置UI界面内容
+extension RecommendViewController {
+    fileprivate func setupUI() {
+        // 1.将uicollectionview添加到控制器的view中
+        view.addSubview(collectionView)
+    }
+}
+
+// MARK:-  遵守UICollectionViewDataSource的数据源协议
+extension RecommendViewController : UICollectionViewDataSource {
+    func numberOfSections(in collectionView: UICollectionView) -> Int {
+        return 12
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        if section == 0 {
+            return 8
+        }
+        return 4
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        // 1.获取cell
+        
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: kNormalCellID, for: indexPath)
+        cell.backgroundColor = UIColor.red
+        return cell
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, viewForSupplementaryElementOfKind kind: String, at indexPath: IndexPath) -> UICollectionReusableView {
+        // 1.取出section的HeaderView
+        let headerView = collectionView.dequeueReusableSupplementaryView(ofKind: kind, withReuseIdentifier: kHeaderViewID, for: indexPath)
+        headerView.backgroundColor = UIColor.green
+        return headerView
+    }
+}
